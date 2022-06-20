cf74b201310b26d023248ff8024f7d6562fdb108
Bishal Sarker <mr.bishalsarker@gmail.com>
2022-06-20 23:10:06 +0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Bincommerz Docs'
+copyright = 'Bincommerz'
+author = 'Bincommerz'
 
 release = '0.1'
 version = '0.1.0'
@@ -30,6 +30,12 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_logo = "bincommerz-logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
